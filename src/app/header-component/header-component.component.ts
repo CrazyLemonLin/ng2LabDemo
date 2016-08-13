262f18e7abed5a2d23b11bb22158dc9410763788
CYL <lemonlinchju@hotmail.com>
2016-08-13 14:24:58 +0800

    練習 Angular 2 元件資料傳遞：使用 @Input() 從外層元件傳資料到內層元件

--- src/app/header-component/header-component.component.ts
+++ src/app/header-component/header-component.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,Input} from '@angular/core';
 
 @Component({
   selector: 'app-header-component',
@@ -7,8 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponentComponent implements OnInit {
 
-  title = 'The Will Will Web !!'
-  titleurl = 'http://blog.miniasp.com/';
+  @Input()
+  title :string;
+  @Input()
+  titleurl :string;
   subtitle = '記載著 Will 在網路世界的學習心得與技術分享';
   num = 10;
   isHide = false;
