8d498a6056101a2a78b0c209dc4b5505f8c24ef2
CYL <lemonlinchju@hotmail.com>
2016-08-13 12:08:48 +0800

    練習Template reference variables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  keyword:string;
+
+  doSearch(event:MouseEvent	| KeyboardEvent,ele:HTMLInputElement){
+
+    if (event instanceof KeyboardEvent ) {
+      var keyboardEvent = event as KeyboardEvent;
+      if (keyboardEvent.keyCode!==13) {
+        return;
+      }
+    }
+    this.keyword=ele.value;
+  }
 }
